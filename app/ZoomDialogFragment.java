2984061ef271d2130d20a3a2ee3d163e0d5f47d3
ruslan <ruslan.gmz@gmail.com>
2016-02-21 14:55:15 +0300

    unfinished realization

---
+++ app/ZoomDialogFragment.java
@@ -0,0 +1,11 @@
+package com.example.ruslan.noteapp;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by rusla on 21.02.2016.
+ */
+public class ZoomDialogFragment extends Fragment{
+
+
+}
